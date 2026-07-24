834da3244d80f9763eb0f83b91dcffcbde1562f0
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-07-24 19:28:54 +0500

    layout improvement

--- src/components/Layout/ArticleCard.jsx
+++ src/components/Layout/ArticleCard.jsx
@@ -17,15 +17,14 @@ export default function ArticleCard({
       }`}
     >
       <div className="w-full">
-        <div className="flex gap-2">
-          <div className="flex flex-col items-center">
-            <span
-              className={`h-3 w-3 rounded-full ${
-                !article.read ? "bg-blue-600" : "bg-gray-400"
-              }`}
-            />
-          </div>
-          <div className=" flex items-center gap-2 text-sm text-gray-500">
+        <div className="mb-2 flex gap-5 items-center">
+          <span
+            className={`h-3 w-3 rounded-full ${
+              !article.read ? "bg-blue-600" : "bg-gray-400"
+            }`}
+          />
+
+          <div className="flex items-center gap-2 text-sm text-gray-500">
             <img src={article.favicon} alt="" className="h-4 w-4 shrink-0" />
 
             <span className="truncate">{article.feedTitle}</span>
@@ -36,32 +35,36 @@ export default function ArticleCard({
           </div>
         </div>
 
-        <div className="mx-10">
-          <button
-            onClick={() => {
-              onSelect;
-            }}
-            className="text-left"
-          >
-            <h3
-              className={`line-clamp-2 text-lg font-semibold ${
-                article.read
-                  ? "font-normal text-gray-500"
-                  : "font-bold text-gray-900"
-              }`}
+        <div className="mx-8 flex flex-col">
+          <div className="flex gap-4 items-center">
+            <button
+              onClick={() => {
+                onSelect;
+              }}
+              className="text-left"
             >
-              {article.title}
-            </h3>
-          </button>
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-              onToggleSaved(article);
-            }}
-            className="mt-1 rounded p-1 text-gray-400 hover:bg-gray-100 hover:text-blue-600"
-          >
-            <FiBookmark className={saved ? "fill-current text-blue-600" : ""} />
-          </button>
+              <h3
+                className={`line-clamp-2 text-lg font-semibold ${
+                  article.read
+                    ? "font-normal text-gray-500"
+                    : "font-bold text-gray-900"
+                }`}
+              >
+                {article.title}
+              </h3>
+            </button>
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                onToggleSaved(article);
+              }}
+              className="mt-1 rounded p-1 text-gray-400 hover:bg-gray-100 hover:text-blue-600"
+            >
+              <FiBookmark
+                className={saved ? "fill-current text-blue-600" : ""}
+              />
+            </button>
+          </div>
 
           <p className="mt-2 line-clamp-2 text-sm text-gray-600">
             {article.description}
