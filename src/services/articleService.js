2edfecacc8fc77d1606e45a51b92387b6aeccd70
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-07-26 16:36:47 +0500

    api fix

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -1,7 +1,7 @@
 export async function getArticles(feedUrl) {
   try {
     const response = await fetch(
-      `${import.meta.env.VITE_API_URL}/articles?url=${encodeURIComponent(feedUrl)}`,
+      `${import.meta.env.VITE_API_URL}/api/articles?url=${encodeURIComponent(feedUrl)}`,
     );
 
     const data = await response.json();
